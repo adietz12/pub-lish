a6ab7702a0fe5069db8f5946f5f6c872bc3a2dce
adietz12 <150217383+adietz12@users.noreply.github.com>
2024-04-04 19:55:06 +0000

    Added top navbar

--- publish.py
+++ publish.py
@@ -1,4 +1,7 @@
+#Use this command to run
 #flask --app publish run
+#This turns on debug mode
+#flask --app publish --debug run
 from flask import Flask, render_template, request, make_response, redirect
 from mongita import MongitaClientDisk
 from bson import ObjectId
