b343fb397ae661270c1acaf4ba3fcb4765e4bd3b
adietz12 <150217383+adietz12@users.noreply.github.com>
2024-04-19 17:07:30 +0000

    Added trending and latest pages

--- create_db.py
+++ create_db.py
@@ -2,12 +2,20 @@
 import json
 from mongita import MongitaClientDisk  
 
+#This one is for dates
+from datetime import datetime
+# Function to convert date string to datetime object
+def convert_date(date_str):
+    # Assuming the date format is 'month/day/year'
+    month, day, year = map(int, date_str.split('/'))
+    return datetime(year, month, day)
+
 #Put future articles in here the way it is formatted
 publish_data = [
-    {"author":"Spongebob", "title":"How I lost my edging streak", "text":"blah blah blah blah"},
-    {"author":"Senator Armstrong", "title":"Why sigma male is better than alpha male", "text":"blah blah blah blah"},
-    {"author":"William Afton", "title":"My Skibidi Toilet addiction", "text":"blah blah blah blah"},
-    {"author":"Guts", "title":"I MET KAI CENAT!! (GONE SEXUAL)", "text":"blah blah blah blah"}
+    {"pop":"1","author":"Spongebob", "title":"How I lost my edging streak", "text":"blah blah blah blah", "date_created":convert_date("3/1/23"), "date_string":"3/1/23"},
+    {"pop":"1","author":"Senator Armstrong", "title":"Why sigma male is better than alpha male", "text":"blah blah blah blah", "date_created":convert_date("5/28/19"), "date_string":"5/28/19"},
+    {"pop":"1","author":"William Afton", "title":"My Skibidi Toilet addiction", "text":"blah blah blah blah", "date_created":convert_date("4/20/24"), "date_string":"4/20/24"},
+    {"pop":"1","author":"Guts", "title":"I MET KAI CENAT!! (GONE SEXUAL)", "text":"blah blah blah blah", "date_created":convert_date("1/11/22"), "date_string":"1/11/22"}
 ]
 
 #Create a mongita client connection
--- publish.py
+++ publish.py
@@ -2,9 +2,10 @@
 #flask --app publish run
 #This turns on debug mode
 #flask --app publish --debug run
-from flask import Flask, render_template, request, make_response, redirect
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from mongita import MongitaClientDisk
 from bson import ObjectId
+
 
 app = Flask(__name__)
 
@@ -35,13 +36,39 @@ def feed():
         item["object"] = ObjectId(item["_id"])
     return render_template("feed.html", data=data)
 
+@app.route("/trending")
+def trending():
+    article_collection = articles_db.article_collection
+    #load data
+    data = list(article_collection.find({}).sort("pop", -1))
+    for item in data:
+        item["_id"] = str(item["_id"])
+        item["object"] = ObjectId(item["_id"])
+    return render_template("feed.html", data=data)
+
+@app.route("/latest")
+def latest():
+
+    #Convert date strings to datetime objects in db
+    article_collection = articles_db.article_collection
+
+    #load data
+    data = list(article_collection.find({}).sort("date_created", -1))
+    for item in data:
+        item["_id"] = str(item["_id"])
+        item["object"] = ObjectId(item["_id"])
+    return render_template("feed.html", data=data)
+
 @app.route("/article/<id>", methods=["GET"])
 def article(id=None):
     if id:
         #open data
         article_collection=articles_db.article_collection
+        
         #get item
         data = article_collection.find_one({"_id": ObjectId(id)})
         data["id"] = str(data["_id"])
         return render_template("article.html", data=data)
-    return render_template("home.html")
+    else:
+        return redirect("/home")
+
