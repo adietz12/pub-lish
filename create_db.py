3b41c2ad7e6c54a6f5bef65c2d25be65f696eaa0
Darien Yoder <116597751+darienyoder@users.noreply.github.com>
2024-04-28 13:21:59 -0400

    Added more articles

--- create_db.py
+++ create_db.py
@@ -168,14 +168,90 @@ As a trans woman, Pelz said LGBTQ+ people and allies should assess their risk be
 “If you can tolerate that risk, start calling representatives, start writing letters and be heard,” Pelz said. “When you hear somebody say something that is blatantly untrue about trans folks and bathrooms, correct them and explain why it matters.”
 """,
 """
+The Kent State of Well-Being encouraged students to take steps toward positive environmental impacts during its meeting Wednesday.
 
-""",]
+The event involved students making their own eco-friendly reusable bags and learning about sustainable ways to dispose of plastic wrappers via ecobricks.
+
+Ecobricks are simple ways to limit the greenhouse gasses that get sent into the atmosphere after plastic sits in the sun. All you need is a dry plastic bottle, dry plastic and a stick for packing to begin creating your own.
+
+Using old recreation center T-shirts, participants were able to tie knots to create a sustainable bag that can be used for class, markets or grocery shopping.
+
+Evan Coates, a junior public health major, said he wanted this event to not only be informative, but also be interactive. He used the ecobrick as an educational factor and the bags as an eye-catcher.
+
+“We had leftover shirts that we were gonna get rid of so I just thought, well, let’s show students what they can turn these into,” Coates said.
+
+Plastic pollution is becoming more of an issue as time goes on. Reusable bags are one of many ways people can limit waste and reduce their footprint.
+
+In addition to plastic waste, reusable bags hold more items than the average grocery store bag. Therefore, the trips you take and the waste you create are all minimized.
+
+Reusable bags in general can be stronger and more durable. Heavy items while grocery shopping, such as milk or soda, typically need to be outside of the plastic bag due to fear of breakage. Reusable bags made of thick and layered material leave you stress-free while carrying in your groceries.
+
+While ecobricks weren’t actively being made at the event, Coates was there to explain more about how to create one, how it’s beneficial, and the impact it could make if everyone takes part in it.
+
+“We wanted to get involved with the community, and I felt like this would spread education about environmental topics,” he said.
+
+The Kent State of Well-Being is located to the right of the campus recreation center entrance. Employees here are referred to as “peer educators,” and work to encourage balance in students’ lives.
+
+Brynn Kler, a sophomore exercise science major and employee for the Kent State of Well-Being, said that wellness is all-encompassing and they try to educate students about that.
+
+The center uses the eight dimensions of wellness when planning events and resources. The eight dimensions are the following: social, emotional, spiritual, intellectual, physical, environmental, financial and occupational.
+
+“We preach a lot about how our wellness is all individual,” Kler said. “You know, my wellness may not look the same as yours, and that is completely normal.”
+
+With wellness and sustainability tied together at this event, students got an educational and hands-on event promoting environmental well-being.
+
+For connection to more resources, visit the Kent State of Well-Being center.
+""",
+"""
+Researchers from the university teamed up with other universities to form the Ohio/Pennsylvania University Research Consortium, which works to summarize the effects of the East Palestine train derailment.
+
+Beyond Kent State, the consortium is made up of around two dozen researchers from seven universities and was organized by researchers from Case Western Reserve University, The Ohio State University and University of Pittsburgh.
+
+Jeffrey Hallam, a university professor in the College of Public Health and director of the Healthy Communities Research Institute, is the liaison between Kent State and the Research Consortium.
+
+Hallam said the universities wished to participate in this research opportunity because of the proximity of the derailment, but researchers further away were also interested in exploring the data.
+
+“The Research Consortium was put together primarily because this happened right there in East Palestine, and there was a flood of people from Ohio and Pennsylvania that wanted to go in there and do research,” Hallam said. “The folks that live there were inundated with people and researchers, lots of people running around while they’re still trying to deal with the cleanup and everything else.”
+
+Hallam said the consortium came together to provide an independent voice of the data that was already being collected and present it to the public.
+
+Members of the consortium have a variety of academic backgrounds and research experience and use pre-obtained data from various sources, such as other researchers and response agencies.
+
+The goal of the consortium is to present the findings in a manner that is more understandable for those affected and to make recommendations for community members, Hallam said.
+
+The data presented by members of the Research Consortium is publicly available, but the university has its own researchers who are currently collecting data separately from the consortium.
+
+“It was all publicly available data that was then presented back to the public, but we do have Kent State researchers that are collecting data and have been since the derailment,” Hallam said.
+
+The Kent State researchers collecting their own data have not yet presented their findings, as the research is ongoing.
+
+Sandra Morgan, strategic partnership and outreach director for the College of Arts and Sciences, looks for opportunities on behalf of the college, which prompted her to look for research for the derailment.
+
+“Out of this tragedy at East Palestine, I saw an opportunity for our faculty members to participate, do some research and get in the mix,” Morgan said.
+
+Other universities were attempting to initially help with conducting research in the city, but Morgan said they were not necessarily helping for the right reasons, and this caused Kent State researchers to take a while building trust and becoming part of the consortium.
+
+Morgan said the research opportunities presented were great, but what inspired her and other members of the university to partake in it was the closeness with which the derailment happened.
+
+“We wanted to know what was happening, certainly, in the area, and especially since we have a couple of campuses that are right in the area, Salem and East Liverpool, we wanted to be a part of the solution,” Morgan said.
+
+Morgan said the researchers are working through all of the data presented to them, including samples from soil, water, air and plant life.
+
+The consortium held a state of the science update in East Palestine on April 10, which focused mainly on the data surrounding air. The next update, which will occur at an undecided future date, will focus on soil and water data.
+
+The state of the science update was open to the public, and residents or anyone else interested in the effects was encouraged to attend.
+
+“To be able to interpret the information in a way that people understood, and for them to be able to ask questions to people who are experts in their respective fields, was also very helpful,” Morgan said. “To not just have the government answer to everything, but to have people for whom this is their life’s work be able to answer.”
+""",
+]
 #Put future articles in here the way it is formatted
 print(article)
 publish_data = [
     {"pop":"1","author":"Lauren Vaughn", "title":"School of Fashion collaborates with ’90s fitness guru for t-shirt design contest", "text":article[0], "date_created":convert_date("4/28/24"), "date_string":"4/28/24"},
-    {"pop":"1","author":"Olivia Montgomery", "title":"Sexual Assault Awareness Month provides support for students and community", "text":article[1], "date_created":convert_date("3/28/24"), "date_string":"3/28/24"}
-    {"pop":"1","author":"Aden Graves", "title":"Inside House Bill 183: Ohio’s latest bathroom bill", "text":article[2], "date_created":convert_date("3/28/24"), "date_string":"3/28/24"}
+    {"pop":"1","author":"Olivia Montgomery", "title":"Sexual Assault Awareness Month provides support for students and community", "text":article[1], "date_created":convert_date("4/28/24"), "date_string":"3/28/24"},
+    {"pop":"1","author":"Aden Graves", "title":"Inside House Bill 183: Ohio’s latest bathroom bill", "text":article[2], "date_created":convert_date("3/28/24"), "date_string":"4/28/24"},
+    {"pop":"1","author":"Morgan Hoover", "title":"Kent State of Well-Being supports Earth Month with Wednesday event", "text":article[3], "date_created":convert_date("4/25/24"), "date_string":"4/25/24"},
+    {"pop":"1","author":"Kayla Gleason", "title":"University researchers analyze, present effects of East Palestine train derailment", "text":article[4], "date_created":convert_date("4/25/24"), "date_string":"4/25/24"},
 ]
 
 #Create a mongita client connection
